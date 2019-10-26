f97ca0a3e9f4ff01d62dd6a96d9bf5bb18870300
smiska <Mihly Sry>
2019-10-26 10:26:37 +0200

    d.ts file, store initializing

--- src/index.tsx
+++ src/index.tsx
@@ -1,41 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { createStore, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+import thunk from 'redux-thunk'
 
-// const App = () => {
-//   return <div></div>
-// }
-interface AppProps {
-  color?: string;
-}
+const store = createStore(reducers, applyMiddleware(thunk))
 
-interface AppState {
-  counter: number;
-}
-
-class App extends React.Component<AppProps, AppState> {
-  constructor(props: AppProps) {
-    super(props)
-    this.state = { counter: 0 }
-  }
-
-  onIncrement(): void {
-    let cnt = this.state.counter
-    this.setState({ counter: ++cnt })
-  }
-
-  onDecrement(): void {
-    let cnt = this.state.counter
-    this.setState({ counter: --cnt })
-  }
-
-  render() {
-    return (
-      <div>
-        <button onClick={this.onIncrement.bind(this)}>Increment</button>
-        <button onClick={this.onDecrement.bind(this)}>Decrement</button>
-        <p>{this.state.counter}</p>
-      </div>)
-  }
-}
-
-ReactDOM.render(<App color="red"></App>, document.querySelector('#root'))
+ReactDOM.render(
+  <Provider store={store}>
+    <App color="red"></App>
+  </Provider>, document.querySelector('#root'))
