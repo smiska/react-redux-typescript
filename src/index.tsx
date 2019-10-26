d09f1b23911c8d0d7b5ec80e4262dbb8e6a261ac
smiska <Mihly Sry>
2019-10-26 10:09:53 +0200

    project setup

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,41 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// const App = () => {
+//   return <div></div>
+// }
+interface AppProps {
+  color?: string;
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+interface AppState {
+  counter: number;
+}
+
+class App extends React.Component<AppProps, AppState> {
+  constructor(props: AppProps) {
+    super(props)
+    this.state = { counter: 0 }
+  }
+
+  onIncrement(): void {
+    let cnt = this.state.counter
+    this.setState({ counter: ++cnt })
+  }
+
+  onDecrement(): void {
+    let cnt = this.state.counter
+    this.setState({ counter: --cnt })
+  }
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.onIncrement.bind(this)}>Increment</button>
+        <button onClick={this.onDecrement.bind(this)}>Decrement</button>
+        <p>{this.state.counter}</p>
+      </div>)
+  }
+}
+
+ReactDOM.render(<App color="red"></App>, document.querySelector('#root'))
